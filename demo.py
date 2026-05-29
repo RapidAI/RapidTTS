d378f753c4cc6ac5e59673df47d4f826d8eead16
SWHL <liekkaskono@163.com>
2026-05-29 16:50:50 +0800

    chore: update demo

--- demo.py
+++ demo.py
@@ -3,14 +3,10 @@
 # @Contact: liekkaskono@163.com
 from rapidtts import RapidTTS, SynthesisRequest, TTSModel
 
-model_root_dir = "rapidtts/models/moss_nano_onnx"
-tts = RapidTTS(model=TTSModel.MOSS_NANO_ONNX, model_root_dir=model_root_dir)
+tts = RapidTTS(model=TTSModel.MOSS_NANO_ONNX)
 
 text = "2026年5月8日，猪肉价格为每斤13.5元，较前期上涨6.3%；车牌号码为京A86F29。"
-ref_audio_path = "rapidtts/models/moss_nano_onnx/assets/audio/zh_6.wav"
-result = tts.synthesize(
-    SynthesisRequest(text=text, extras={"prompt_audio_path": ref_audio_path})
-)
+result = tts.synthesize(SynthesisRequest(text=text))
 
 save_path = "outputs/moss_nano_result.wav"
 result.save(save_path)
